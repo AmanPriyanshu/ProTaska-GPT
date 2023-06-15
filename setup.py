ff9f8dd5eca56e3274479355ce5b47e5bd0c2629
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2023-06-16 00:40:25 +0300

    fixed setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='ProTaska-GPT',
     version='0.0.1',
     description='Unleash the Potential of Datasets with Intelligent Tasks, Tutorials, and Algorithm Recommendations.',
+    long_description='ProTaska-GPT is an open-source Python library that empowers data scientists and researchers to unleash the potential of datasets with intelligent tasks, tutorials, and algorithm recommendations. It seamlessly integrates with popular dataset sources like Kaggle and Hugging Face, allowing users to easily import and work with diverse datasets. With ProTaska-GPT, you can visualize and explore datasets, gain insights into data distributions, correlations, and missing values, and generate customized tasks and algorithm suggestions based on your dataset characteristics. Whether you are a beginner or an experienced practitioner, ProTaska-GPT provides a collection of beginner-friendly tutorials that guide you through common data science workflows, fostering practical learning and skill development.',
     author='Aman Priyanshu, Supriti Vijay',
     author_email='amanpriyanshusms2001@gmail.com',
     packages=['protaska'],
