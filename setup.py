ad1313c42b1458d933d7b325c971c6bcf5725ab7
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2023-06-17 19:39:02 +0300

    cleaned lib

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='ProTaska-GPT',
-    version='0.0.4',
+    version='0.0.5',
     description='Unleash the Potential of Datasets with Intelligent Tasks, Tutorials, and Algorithm Recommendations.',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -20,6 +20,8 @@ setup(
         'colorama',
         'wikipedia',
         'openai',
+        'datasets',
+        'tiktoken'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
